6b449c65b0f59a53f08d38ba4cbe202fc288137d
cosm1cus <sergei20102010@gmail.com>
2025-10-03 23:15:45 +0000

    add symbol_count function

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from stats import word_count
+from stats import word_count, symbol_count
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_boot_text(book_path)
     count = word_count(text)
+    symbol = symbol_count(text)
     print(text)
     print(f"Found {count} total words")
+    print(symbol)
 
 def get_boot_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def word_count(book):
     word_list = book.split()
     return len(word_list)
+
+def symbol_count(text):
+    book_text = text.lower()
+    dict_symbol = {}
+    for i in book_text:
+        if i not in dict_symbol:
+            dict_symbol[i] = 1  
+        else:
+            dict_symbol[i] = dict_symbol[i] + 1
+    return dict_symbol
