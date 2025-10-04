529f9de002645c516c07a95b4d57f4ef3a220dcc
cosm1cus <sergei20102010@gmail.com>
2025-10-04 00:36:33 +0000

    add sys module to recoginze path

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from stats import word_count, symbol_count, sort_symbol_count, sort_symbol_count
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv
+    if len(book_path) <= 1:
+        raise Exception("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     text = get_boot_text(book_path)
     count = word_count(text)
     symbol = symbol_count(text)
     sorted_symbol = sort_symbol_count(symbol)
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
+    print(f"Analyzing book found at {book_path[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {count} total words")
     print("--------- Character Count -------")
@@ -16,7 +20,7 @@ def main():
     print("============= END ===============")
 
 def get_boot_text(path):
-    with open(path) as f:
+    with open(path[1]) as f:
         return f.read()
 
 main()
