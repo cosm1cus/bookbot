571621e67aa05d5c939449bafb7160407a2388a9
cosm1cus <sergei20102010@gmail.com>
2025-10-04 00:15:47 +0000

    add sort_symbol_count function & update main.py output

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from stats import word_count, symbol_count
+from stats import word_count, symbol_count, sort_symbol_count, sort_symbol_count
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_boot_text(book_path)
     count = word_count(text)
     symbol = symbol_count(text)
-    print(text)
+    sorted_symbol = sort_symbol_count(symbol)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {count} total words")
-    print(symbol)
+    print("--------- Character Count -------")
+    for i in sorted_symbol:
+        print(f"{i["char"]}: {i["num"]}")
+    print("============= END ===============")
 
 def get_boot_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -11,3 +11,17 @@ def symbol_count(text):
         else:
             dict_symbol[i] = dict_symbol[i] + 1
     return dict_symbol
+
+def sort_on(item):
+    return item["num"]
+
+def sort_symbol_count(raw_text):
+    sorted_symbol_tuple = []
+    for i in raw_text:
+        char = i
+        num = raw_text[i]
+        if char.isalpha() == True:
+            combined_dict = {"char":char, "num":num}
+            sorted_symbol_tuple.append(combined_dict)
+    sorted_symbol_tuple.sort(reverse=True, key=sort_on)
+    return sorted_symbol_tuple
